3d79cb9e5766bba0caa7aa131e9d675c464b9650
bassblitzed <Bassblitzed@DESKTOP-TK73GES>
2021-11-28 00:33:07 -0500

    Added Recipes for dildoium and amongium along with small fixes

---
+++ src/main/java/net/mcreator/sussyballs/item/DildoiumIngotItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.sussyballs.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.sussyballs.SussyBallsModElements;
+
+@SussyBallsModElements.ModElement.Tag
+public class DildoiumIngotItem extends SussyBallsModElements.ModElement {
+	@ObjectHolder("sussy_balls:dildoium_ingot")
+	public static final Item block = null;
+	public DildoiumIngotItem(SussyBallsModElements instance) {
+		super(instance, 11);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).isImmuneToFire().rarity(Rarity.EPIC));
+			setRegistryName("dildoium_ingot");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/sussyballs/procedures/AmongUsThemeSongRightClickedOnBlockProcedure.java
@@ -0,0 +1,8 @@
+package net.mcreator.sussyballs.procedures;
+
+import java.util.Map;
+
+public class AmongUsThemeSongRightClickedOnBlockProcedure {
+	public static void executeProcedure(Map<String, Object> dependencies) {
+	}
+}
