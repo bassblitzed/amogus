3d5cc3aea375f64c48ec8f0b7e517072ff07d076
bassblitzed <Bassblitzed@DESKTOP-TK73GES>
2021-11-27 23:52:51 -0500

    added sussy baka record and a few other things

--- src/main/java/net/mcreator/sussyballs/SussyBallsModElements.java
+++ src/main/java/net/mcreator/sussyballs/SussyBallsModElements.java
@@ -45,6 +45,8 @@ public class SussyBallsModElements {
 	public final List<Supplier<Enchantment>> enchantments = new ArrayList<>();
 	public static Map<ResourceLocation, net.minecraft.util.SoundEvent> sounds = new HashMap<>();
 	public SussyBallsModElements() {
+		sounds.put(new ResourceLocation("sussy_balls", "amongustrapremix"),
+				new net.minecraft.util.SoundEvent(new ResourceLocation("sussy_balls", "amongustrapremix")));
 		try {
 			ModFileScanData modFileInfo = ModList.get().getModFileById("sussy_balls").getFile().getScanResult();
 			Set<ModFileScanData.AnnotationData> annotations = modFileInfo.getAnnotations();
